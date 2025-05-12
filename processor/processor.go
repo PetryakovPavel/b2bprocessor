2d3a18b4e3380b47d46c011a320d3ac469616a1a
PetryakovPavel <petryakovpavelvl@gmail.com>
2025-05-12 13:51:34 +0300

    Update processor.go

--- processor/processor.go
+++ processor/processor.go
@@ -9,7 +9,7 @@ import (
     "go.opentelemetry.io/collector/pdata/ptrace"
     "go.uber.org/zap"
 
-    "github.com/your_username/b2bprocessor"
+    "github.com/PetryakovPavel/b2bprocessor"
 )
 
 type DynamicBatchProcessor struct {
